9844a92824cada4cb478173294d5dcbe93f217b3
Eteimz <8teims@gmail.com>
2021-06-18 00:14:10 +0100

    Correcting translation

--- chat.py
+++ chat.py
@@ -41,4 +41,4 @@ while True:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
     else:
-        print(f"{bot_name}: I do not understand...")
+        print(f"{bot_name}: Yo no comprendo...")
